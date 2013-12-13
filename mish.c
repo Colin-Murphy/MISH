1cc06325f50e5624045a197de3c6b3daae6fb69e
test <colinmurphy1993@gmail.com>
2013-12-13 00:36:15 -0500

    Created files, time to get this thing started

---
+++ mish.c
@@ -0,0 +1,11 @@
+#include "mish.h"
+
+int main(int argc, char *argv[]) {
+	
+	while (!feof(stdin)) {
+		break;
+		
+	}
+	
+	printf("Done\n");
+}
---
+++ mish.h
@@ -0,0 +1,7 @@
+//Mish.h
+//The header for cs243 project3 mish
+//Author Colin L Murphy clm3888@rit.edu
+//////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
