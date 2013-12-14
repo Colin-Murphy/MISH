991a744355151771e78bb120a36a0b2d4caebc07
test <colinmurphy1993@gmail.com>
2013-12-14 05:04:50 -0500

    Finished the project, met all requirements (although I probably missed something)... wow its kinda late

--- mish.c
+++ mish.c
@@ -1,14 +1,15 @@
 #include "mish.h"
 
+char *commandHistory[1024];
+int count = 0;
+bool verb = false;
+
 int main(int argc, char *argv[]) {
 	
-	int count = 0;
-	char *line;
 	size_t maxLine = 1024;
+	char *line = NULL;
 	line = (char *)malloc( maxLine );
-	char *history[maxLine];
 	int bytesRead = 0;
-	bool verbose = false;
 	while (!feof(stdin)) {
 		printf("mish[%d]> ",count+1);
 		bytesRead = getline(&line, &maxLine, stdin);
@@ -27,57 +28,41 @@ int main(int argc, char *argv[]) {
 		if (tok >0) {
 			//Internal command: quit
 			if (strcmp(tokens[0], "quit") == 0) {
-				for (int i=0; i<tok; i++) {
-					free(tokens[i]);
-				}
-				
-				for (int i=0; i<count; i++) {
-					free(history[i]);
-				}
+				quit(tok,tokens);
 				break;
 			}
 			
 			//Internal command: help
 			else if (strcmp(tokens[0],"help") ==0) {
-				printf("verbose on/off: turn the shell verbose on or off.\n");
-				printf("help: print a list of internal commands.\n");
-				printf("history: print a list of commands executed so far, including their arguments.\n");
-				printf("quit: cleans up memory and gracefully terminate mish.\n");
+				help(tok, tokens);
 			}
 			//Internal command: history
 			else if (strcmp(tokens[0],"history") ==0) {
-				printf("Command History\n");
-				for (int i = 0; i<count; i++) {
-					printf("\t mish[%d] > %s",i, history[i]);
-				}
+				history(tok, tokens);
 			}
 			
 			//Internal command: verbose
 			else if (strcmp(tokens[0],"verbose") ==0) {
-				if (strcmp(tokens[1],"on") == 0) {
-					verbose = true;
-				}
-				
-				else if (strcmp(tokens[1],"off") == 0) {
-					verbose = false;
-				}
-				
-				else {
-					perror("Invalid state for verbose\n");
-				}
+				verb = verbose(tok, tokens);
 				
 			}
 			
 			//External commands
 			else {
-				if (verbose) {
+				//handle verbose printing
+				if (verb) {
 					printf("\tcommand: %s\n",line);
 					printf("\tinput command tokens:\n");
 					
 					for (int i = 0; i< tok; i++) {
 						printf("\t%d: %s.\n",i,tokens[i]);
 					}
-				}	
+				}
+				
+				//run the external command
+				external(tok, tokens);	
+
+	
 			}
 			
 			
@@ -85,15 +70,15 @@ int main(int argc, char *argv[]) {
 			for (int i=0; i<tok; i++) {
 				free(tokens[i]);
 			}
+			
+			//Update the command history
+			commandHistory[count] = malloc(strlen(line)+1);
+			strcpy(commandHistory[count],line);
+			count++;
 		}
-		history[count] = malloc(strlen(line)+1);
-		strcpy(history[count],line);
-		count++;
 
 	}
 	
-	
-	printf("Goodbye\n");
 }
 
 //Break the string into tokens while preserving text in single or double quotes
@@ -101,10 +86,6 @@ int main(int argc, char *argv[]) {
 //param tokens: the array of tokens to store into
 //return int: the number of tokens found
 int tokenize(char *line, char *tokens[]) {
-	
-	//tokens[0] = malloc(sizeof("Hello") + 1);
-	//strcpy(tokens[0], "Hello");
-	
 	int len = strlen(line);
 	int pos = 0;
 	int nextQuote = -1;
@@ -114,9 +95,7 @@ int tokenize(char *line, char *tokens[]) {
 
 	
 	while (pos < len) {
-		
-		//printf("Working on string from %d to %d\n",pos, len);
-		
+		//Break up all text leading up to the first " or '
 		if (line[pos] != '\"' && line[pos] !='\'') {
 			
 			char *nQuote = strchr(line + pos, '\"');
@@ -159,10 +138,12 @@ int tokenize(char *line, char *tokens[]) {
 				token = strtok (NULL, " \n");
 			}
 			
-			
+			//free the memory
 			free(substring);
+			//update the position
 			pos += stop;
-
+			
+			//reset these so the function can run again properly
 			nextQuote = -1;
 			nextApos = -1;
 					
@@ -171,10 +152,13 @@ int tokenize(char *line, char *tokens[]) {
 		
 		//line[pos] is a double or single quote
 		else {
+			//skip the first character since we don't want the " or '
 			int start = pos + 1;
+			//The location of the corresponding charcter (" or ') relative to pos
 			int stop;
 			//Found quote
 			if (line[pos] == '\"') {
+				//Find the next " and mark that as the stop point
 				char *nQuote = strchr(line + pos + 1, '\"');
 				if (nQuote) {
 					nextQuote = (int)(nQuote - (line + pos));
@@ -226,5 +210,132 @@ int tokenize(char *line, char *tokens[]) {
 		
 				
 	}
+	//return the number of tokens parsed
 	return count;
+}
+
+//Run external commands
+//param argc: number of tokens
+//param argv: tokens parsed
+//return int: the status returned by the command, or -1 on error
+int external( int argc, char **argv) {
+	pid_t id, my_id;
+	int status;
+	
+	id = fork();
+	switch( id ) {
+
+	//Error :(
+	case -1:
+		perror( "fork" );
+		exit( EXIT_FAILURE );
+	
+	//in child process
+	case 0:
+
+		// report our identity
+		my_id = getpid();
+		
+		//Add a null token so execvp works
+		argv[argc] = NULL;
+		
+		if (verb) {
+			printf("\twait for pid %d: %s\n",my_id, argv[0]);
+			printf("\texecvp: %s\n",argv[0]);
+		}
+
+		execvp( argv[0], argv );
+
+		//print the error
+		perror( "execvp" );
+
+		//give up and exit
+		_exit( EXIT_FAILURE );
+
+		break;
+
+	//parent
+	default:
+		break;
+
+	}
+
+	// parent will wait for child to exit
+	id = wait( &status );
+	if( id < 0 ) {
+		perror( "wait" );
+		//wort happen, but this return keeps the compiler happy
+		return -1;
+	}
+	else {
+		if (status != 0) {
+			printf("Status is: %d\n",status);
+		}
+		
+		return status;
+	}
+	
+	
+}
+//Prints the help menu
+//param argc: number of tokens (unused)
+//param argv: tokens parsed (unused)
+//return int: unused, but the spec requires it
+int help( int argc, char ** argv ) {
+	printf("Mish Internal Commands\n");
+	printf("\tverbose on/off: turn the shell verbose on or off.\n");
+	printf("\thelp: print a list of internal commands.\n");
+	printf("\thistory: print a list of commands executed so far, including their arguments.\n");
+	printf("\tquit: cleans up memory and gracefully terminates mish.\n");
+	
+	return 0;	
+}
+
+//Toggles verbose
+//param argc: number of tokens
+//param argv: tokens parsed
+//return int: whether verbose is on or off
+int verbose( int argc, char **argv ) {
+	
+	if (strcmp(argv[1],"on") == 0) {
+		verb = true;
+	}
+	
+	else if (strcmp(argv[1],"off") == 0) {
+		verb = false;
+	}
+	
+	else {
+		perror("Invalid state for verbose\n");
+	}
+	
+	
+	return verb;	
+}
+//Prints the command history
+//param argc: number of tokens (unused)
+//param argv: tokens parsed (unused)
+//return int: unused, but the spec requires it
+int history( int argc, char **argv ) {
+	printf("Command History\n");
+	for (int i = 0; i<count; i++) {
+		printf("\t mish[%d] > %s",i, commandHistory[i]);
+	}
+	
+	return 0;
+}
+//Quits mish
+//param argc: number of tokens
+//param argv: tokens parsed
+//return int: unused, but the spec requires it
+int quit( int argc, char **argv ) {
+	//Clean up all the allocated memory
+	for (int i=0; i<argc; i++) {
+		free(argv[i]);
+	}
+	
+	for (int i=0; i<count; i++) {
+		free(commandHistory[i]);
+	}
+	return 0;
 }
--- mish.h
+++ mish.h
@@ -3,5 +3,19 @@
 //Author Colin L Murphy clm3888@rit.edu
 //////////////////////////////////////////////////
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int tokenize(char *line, char *tokens[]);
+int help( int argc, char ** argv );
+int history( int argc, char ** argv );
+int verbose( int argc, char **argv );
+int quit( int argc, char **argv );
+int external( int argc, char **argv);
